39c882befec47d2c551a3b0daf430ad12bf22d74
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-10-12 22:32:19 -0700

    Uncomment out rectangle and line examples.

--- canvas.js
+++ canvas.js
@@ -4,27 +4,27 @@ canvas.height = window.innerHeight;
 
 // c stands for context
 let c = canvas.getContext('2d');
-c.fillStyle = '#333';
-// c.fillRect(50, 100, 300, 300);
-// c.fillStyle = '#000';
-// c.fillRect(200, 150, 300, 300);
-// console.log(canvas);
+c.fillStyle = 'lightgrey';
+c.fillRect(50, 100, 300, 300);
+c.fillStyle = '#000';
+c.fillRect(200, 150, 300, 300);
+console.log(canvas);
 
 // Line
-// c.beginPath(); // separates this line from the rect above
-// c.moveTo(50, 300);
-// c.lineTo(300, 100);
-// c.lineTo(349, 450);
-// c.strokeStyle = "#fff";
-// c.stroke();
+c.beginPath(); // separates this line from the rect above
+c.moveTo(50, 300);
+c.lineTo(300, 100);
+c.lineTo(349, 450);
+c.strokeStyle = "#fff";
+c.stroke();
 
 // Arc / Circle
 // startAngle = at what angle would we like to start drawing our arc (radian value)
 // endAngle = how long would we like the arc angle to go on for (radian value)
 // .arc(x, y, radius, startAngle, endAngle, drawCounterClockwise:Bool)
-// c.beginPath(); //separates this circle from line above
-// c.arc(250, 200, 30, 0, Math.PI*2, false);
-// c.stroke();
+c.beginPath(); //separates this circle from line above
+c.arc(250, 200, 30, 0, Math.PI*2, false);
+c.stroke();
 
 for(let i = 0; i < 20; i++){
   //use Math.random() to randomize coordinants;
